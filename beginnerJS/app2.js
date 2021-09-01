c5a96173351376dc8799320f0ec03bf33cc03078
YunGyeong Kim <86589216+YunGyeongee@users.noreply.github.com>
2021-09-01 13:48:29 +0900

    바닐라JS #2.5

---
+++ beginnerJS/app2.js
@@ -0,0 +1,4 @@
+const amIFat = null; // null 과 false는 다름
+let something;
+console.log(something, amIFat);
+
---
+++ beginnerJS/app3.js
@@ -0,0 +1,10 @@
+/* 배열 */
+const week = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+const nonsense = [1, 2, "hello", false, null, true, undefined, "Rachel"]
+
+// Get Item from Array
+console.log(week);
+
+// Add one more day to the array
+week.push('sun');
+console.log(week);
