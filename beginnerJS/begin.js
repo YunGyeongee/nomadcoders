2706fd607e01dc2c8779fb9e9f3bc92bc7f8663b
YunGyeong Kim <86589216+YunGyeongee@users.noreply.github.com>
2021-09-02 14:22:12 +0900

    바닐라JS #3.6

--- beginnerJS/begin.js
+++ beginnerJS/begin.js
@@ -8,39 +8,16 @@ const h1 = document.querySelector("div.hello:first-child h1"); // 똑같은게 �
 //title.innerText = "Hello";
 
 function handleTitleClick(){
+    const currentColor = h1.style.color;
     //console.log("title was clicked!");
-    h1.style.color = "blue";
+    if(currentColor === "blue"){
+        newColor ="tomato";
+    } else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
 
-function handleMouseEnter(){
-    h1.innerText = "Mouse is here!";
-}
 
-function handleMouseLeave(){
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier");
-}
-
-function handleWindowOffline(){
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline(){
-    alert("ALL GOOD");
-}
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
