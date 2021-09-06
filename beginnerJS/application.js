9bec5aee735f653a01869fa93731faa56b774910
YunGyeong Kim <86589216+YunGyeongee@users.noreply.github.com>
2021-09-06 19:35:34 +0900

    바닐라JS #4 ㅇㄹ

--- beginnerJS/application.js
+++ beginnerJS/application.js
@@ -2,19 +2,34 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
+// 반복되는 string들을 대문자 변수로 저장해놓는 것이 좋음 -> 오타 방지
 const HIDDEN_CALSSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CALSSNAME);
     const username = loginInput.value; 
     //console.log(username);
-    localStorage.setItem("username", username);
+    localStorage.setItem(USERNAME_KEY, username);
     //greeting.innerText = "Hello " + username;
-    greeting.innerText = `hello ${username}`; // '' 홑따옴표 아님 `` 백틱
+    //greeting.innerText = `hello ${username}`; // '' 홑따옴표 아님 `` 백틱
+    paintGrettings(username);
+}
 
+
+function paintGrettings(username){
+    greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CALSSNAME);
 }
 
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if(savedUsername === null){
+    // show the form
+    loginForm.classList.remove(HIDDEN_CALSSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the greetings
+    paintGrettings(username);
+}
